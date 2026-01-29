2e477151b783370058d110f84af9a42de5f3af6e
NV-7 <naviboun@gmail.com>
2026-01-29 15:10:41 -0600

    Allows the player to drop fruit
    Fruit can be dropped and instantiates a new fruit game object

--- Assets/PlayerBehavior.cs
+++ Assets/PlayerBehavior.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class PlayerBehavior : MonoBehaviour
 {
-    public float speed;
+    // Start is called before the first frame update
+    public float speed = 3f;
     public GameObject fruitHeld;
+    public GameObject[] fruits;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -19,7 +21,7 @@ public class Player : MonoBehaviour
         if (fruitHeld != null)
         {
             Vector3 playerPos = transform.position;
-            Vector3 fruitPos = new Vector3(0.0f, -1.0f, 0.0f);
+            Vector3 fruitPos = new Vector3(0.0f, -6.0f, 0.0f);
 
             fruitHeld.transform.position = transform.position;
 
@@ -31,23 +33,40 @@ public class Player : MonoBehaviour
             newPos.x = newPos.x - speed * Time.deltaTime;
             transform.position = newPos;
         }
-        else if (Keyboard.current.rightArrowKey.isPressed)
+        if (Keyboard.current.rightArrowKey.isPressed)
         {
             Vector3 newPos = transform.position;
             newPos.x = newPos.x + speed * Time.deltaTime;
             transform.position = newPos;
         }
-        else if (Keyboard.current.upArrowKey.isPressed)
+        if (Keyboard.current.upArrowKey.isPressed)
         {
             Vector3 newPos = transform.position;
             newPos.y = newPos.y + speed * Time.deltaTime;
             transform.position = newPos;
         }
-        else if (Keyboard.current.downArrowKey.isPressed)
+        if (Keyboard.current.downArrowKey.isPressed)
         {
             Vector3 newPos = transform.position;
             newPos.y = newPos.y - speed * Time.deltaTime;
             transform.position = newPos;
+        }
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            int num = Random.Range(0, fruits.Length);
+
+
+            Rigidbody2D rb = fruitHeld.GetComponent<Rigidbody2D>();
+            rb.gravityScale = 1.0f;
+
+            Collider2D coll = fruitHeld.GetComponent<Collider2D>();
+            coll.enabled = true;
+            
+            fruitHeld = Instantiate(fruits[num], transform.position, Quaternion.identity);
+
+            fruitHeld.GetComponent<Rigidbody2D>().gravityScale = 0;
+            fruitHeld.GetComponent<Collider2D>().enabled = false;
+
         }
     }
 }
