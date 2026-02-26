e8f46b17464b94aa1a701b6dee3ec3c084733185
NV-7 <naviboun@gmail.com>
2026-02-26 13:48:32 -0600

    ghji

--- Assets/Scripts/dessertBehavior.cs
+++ Assets/Scripts/dessertBehavior.cs
@@ -13,7 +13,7 @@ public class dessertBehavior : MonoBehaviour
     public int points;
     public GameObject nextDessert;
     public GameObject player;
-    public 
+
     
     // Start is called before the first frame update
     void Start()
